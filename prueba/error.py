e671479a2e76683bd058975e662af6b621083a95
Xisap1 <161665534+Xisap1@users.noreply.github.com>
2024-05-15 16:21:27 -0400

    Add files via upload

---
+++ prueba/error.py
@@ -0,0 +1,12 @@
+from anuncio import Campaña
+from campaña import cambiar_campaña
+
+cambio_campaña => 250
+while True:
+    cantidad_letra = input('Ingrese nuevo nombre de campaña: ')
+    try:
+        cantidad_letra = int(cantidad_letra)
+        except
+           print('lo siento, exede la cantidad maxima de letras permitidas')
+        continue
+
